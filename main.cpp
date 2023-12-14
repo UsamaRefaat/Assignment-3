8a7bb9fec7c136af19237f1c30be6ce103ed4464
Osama <117200884+UsamaRefaat@users.noreply.github.com>
2023-12-14 21:01:06 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+// Class definition for XO_App class
+// Author:  Mohammad El-Ramly
+// Date:    10/10/2022
+// Version: 1
+
+#include <iostream>
+#include"pyramid_X_O.h"
+using namespace std;
+
+int main() {
+    int choice;
+    Player* players[2];
+    players[0] = new Player(1, 'x');
+
+    cout << "Welcome to FCAI X-O Game. :)\n";
+    cout << "Press 1 if you want to play with computer: ";
+    cin >> choice;
+    if (choice != 1)
+        players[1] = new Player(2, 'o');
+    else
+        players[1] = new AIUnbeatablePlayer('o',new pyramid_Board);
+
+    GameManager x_o_game(new pyramid_Board(), players);
+    x_o_game.run();
+    delete players[0];
+    delete players[1];
+    system("pause");
+}
+
