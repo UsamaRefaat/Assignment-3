1d3ec5ca298b06cfbfd72cb3f2611ffcaef40a02
Osama <117200884+UsamaRefaat@users.noreply.github.com>
2023-12-18 23:35:44 +0200

    Update GameManager.cpp

--- Four_in_a_row/GameManager.cpp
+++ Four_in_a_row/GameManager.cpp
@@ -1,38 +1,29 @@
-// Class definition for XO_GameManager class
+// Class definition for XO_App class
 // Author:  Mohammad El-Ramly
 // Date:    10/10/2022
 // Version: 1
-
+# include "windows.h"
 #include <iostream>
-#include"include/BoardGame_Classes.hpp"
+#include"include/connectfour.hpp"
 using namespace std;
 
-GameManager::GameManager(Board* bPtr, Player* playerPtr[2] ) {
-    boardPtr = bPtr;
-    players[0] = playerPtr[0];
-    players[1] = playerPtr[1];
-}
+int main(){
+    int choice;
+    Player* players[2];
+    players[0] = new Player (1, 'x');
 
-void GameManager::run() {
-    int  y;
+    cout << "Welcome to FCAI X-O Game. :)\n";
+    cout << "Press 1 if you want to play with computer  : ";
+    cin >> choice;
 
-    boardPtr->display_board();
 
-    while (!boardPtr->game_is_over()) {
-        for (int i:{0,1}) {
-            players[i]->get_move(y);
-            while (!boardPtr->update_board ( y, players[i]->get_symbol())){
-                players[i]->get_move(y);
-            }
-            boardPtr->display_board();
-            if (boardPtr->is_winner()){
-                cout  << players[i]->to_string() << " wins\n";
-                return;
-            }
-            if (boardPtr->is_draw()){
-                cout << "Draw!\n";
-                return;
-            }
-        }
-    }
+    if (choice != 1)
+        players[1] = new connect4Player (2, 'o');
+    else
+        //Player pointer points to child
+        players[1] = new RandomPlayer ('o', 7);
+
+    GameManager x_o_game (new Connect4board() , players);
+    x_o_game.run();
+    system ("pause") ;
 }
